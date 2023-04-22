872fae31dd5dbade5d238305e180d6c1c5225597
Christopher Kennedy <kennednu@bc.edu>
2023-04-22 18:27:26 -0400

    Initial Commit

---
+++ SportsFinder/SportsFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsFinderApp.swift
+//  SportsFinder
+//
+//  Created by Christopher Kennedy on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SportsFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
